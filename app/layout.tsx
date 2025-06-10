b322a7cb948c84d750500969c2f2b9d1230e1dc4
ainponce <aiponce@alumno.unlam.edu.ar>
2025-06-10 10:00:53 -0300

    add favicon and fix tab

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,25 @@
 import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
 import './globals.css'
 
+const inter = Inter({ subsets: ['latin'] })
+
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Ain Moises Ponce | Portfolio',
+  description: 'Portfolio de Ain Moises Ponce',
+  icons: {
+    icon: '/favicon-96x96.png',
+  },
 }
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode
-}>) {
+}) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   )
 }

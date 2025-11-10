94d22474500cb5cdf66a9980ff1699604e1a37df
ainponcesoofia <aponce@soofia.app>
2025-11-10 16:43:28 -0300

    hotfix

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,15 +8,32 @@ const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://ainponce.com'
 
 export const metadata: Metadata = {
   metadataBase: new URL(siteUrl),
+  title: 'Ain Ponce',
   icons: {
     icon: '/favicon-96x96.png',
   },
   openGraph: {
-    images: ['/opengraph-image.png'],
+    title: 'Ain Ponce',
+    images: [
+      {
+        url: '/opengraph-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'Ain Ponce Portfolio',
+      },
+    ],
   },
   twitter: {
     card: 'summary_large_image',
-    images: ['/twitter-image.png'],
+    title: 'Ain Ponce',
+    images: [
+      {
+        url: '/opengraph-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'Ain Ponce Portfolio',
+      },
+    ],
   },
 }
 
